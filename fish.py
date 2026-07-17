9b94307a92b8c602eae8bac8d289c19bc81bd8a6
istoocold <son.of.a.revolver@gmail.com>
2026-07-17 23:46:38 +0530

    make a canvas

--- fish.py
+++ fish.py
@@ -5,9 +5,34 @@ root.title("fish")
 
 root.geometry("400x400")
 
+
+fish = tk.Label(root, text="text")
+x =0
+y =0
+
+for i in range(10):
+    fish.place(x = x, y= y)
+    y = y + 10
+    x =  x +10
+    fish.place(x =x , y=y)
+    
+
+    
+   
+
+
+
+  
+
+    
+
+
+
+    
+    
+   
 root.mainloop()
 
-fish = tk.Label(root, text="text").pack()
 
 
 
