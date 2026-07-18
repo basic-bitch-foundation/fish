6d6fed2efdeff2fd23e35cdf192b2edc0ebdee58
istoocold <son.of.a.revolver@gmail.com>
2026-07-18 23:16:10 +0530

    Update fish.py
    make moving label using pyside6 inside the window

--- fish.py
+++ fish.py
@@ -1,12 +1,34 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget , QLabel
+from PySide6.QtCore import QTimer
 app = QApplication(sys.argv)
 window = QWidget()
 window.show()
-label = QLabel("hello world")
-label.setParent(window)
+window.resize(400, 300)
+
+label = QLabel("hello world", window)
+
+
+
 label.show()
+x =0
+y = 0
+
+
+def move():
+    global x, y
+    x += 1
+    y += 1
+    label.move(x,y)
+
+move()
+timer = QTimer()
+timer.timeout.connect(move)
+timer.start(30)
+    
 app.exec()
+
+
 
 
 
