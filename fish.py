bd462fe270eda03e7dc3b8e89f520da96dd08d67
istoocold <son.of.a.revolver@gmail.com>
2026-07-23 23:43:30 +0530

    added moving fish using velocity instead of coordinate and added boundary for moving constraint

--- fish.py
+++ fish.py
@@ -21,22 +21,36 @@ fish.move(0,0)
 window.show()
 x = 0
 y = 0
+vx = 5
+vy = 3
 
-
+fish.move(x,y)
 def move():
-    global x,y,vy,vx
-   
-    
+    global x,y,vx,vy
+    x += vx
+    y += vy
+    if x <= 0:
+        x =0
+        vx= -vx
+    elif x  + fish.width() >= window.width():
+        x = window.width() - fish.width()
+        vx = -vx
 
+    if y <= 0:
+        y =0
+        vy =-vy
+    elif y + fish.height() >= window.height():
+        y= window.height() -fish.height
+        vy = -vy
     
+    fish.move(x,y)
+
 move()
 
 timer = QTimer()
 timer.timeout.connect(move)
 timer.start(16)
 
-app.exec()
 
 
-
-
+sys.exit(app.exec())
