a2ec7873a4d975568384b20f10e0c5c61e678b30
istoocold <son.of.a.revolver@gmail.com>
2026-07-23 20:59:00 +0530

    tried to import png and show though didn't work for some reason

--- fish.py
+++ fish.py
@@ -1,32 +1,32 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget , QLabel
 from PySide6.QtCore import QTimer
+from PySide6.QtGui import QPixmap
+
 app = QApplication(sys.argv)
 window = QWidget()
-window.show()
+pixmap = QPixmap("fish.png")
+
 window.resize(400, 300)
 
-label = QLabel("hello world", window)
+if pixmap.isNull():
+    print("no fihh broo")
+    sys.exit()
 
 
+fish = QLabel(window)
+fish.setPixmap(pixmap)
+fish.resize(pixmap.size())
+fish.move(0,0)
+window.show()
 x = 0
-y = 0000
+y = 0
 
-window.show
 def move():
-    global x , y
-    x += 2
-    y+=2
-    window.move(x,y)
-
-
-
-
-move()
-timer = QTimer()
-timer.timeout.connect(move)
-timer.start(16)
-    
-app.exec()
+    global x,y
+    timer = QTimer()
+    timer.timeout.connect(move)
+    timer.start(16)
+    app.exec()
 
 
